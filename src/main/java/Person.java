8d7cc918ab8f9390b7825c9980e0b7a47587ffb8
Mirbek <mirbek_karimberdiev@mail.ru>
2023-03-28 11:38:32 +0300

    first commit

---
+++ src/main/java/Person.java
@@ -0,0 +1,65 @@
+import java.util.OptionalInt;
+
+public class Person {
+    protected final String name;
+    protected final String surname;
+    protected int age;
+    protected String address;
+
+    public Person(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public Person(String name, String surname, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    public boolean hasAge() {
+        return age > 0;
+    }
+
+    public boolean hasAddress() {
+        return address != null;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public OptionalInt getAge() {
+        return age == 0 ? OptionalInt.empty() : OptionalInt.of(age);
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void happyBirthday() {
+        age = age + 1;
+    }
+
+    @Override
+    public String toString() {
+        return surname + " " + name;
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode() * surname.hashCode();
+    }
+
+    public PersonBuilder newChildBuilder() {
+        return new PersonBuilder().setSurname(surname);
+    }
+}
